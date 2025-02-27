d502c2acf1a5d42466cbfdc4bc98c88ca69d000f
Archit Kakkar <education.archit@gmail.com>
2025-02-28 01:05:58 +0530

    feat: header section completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <>
-      <h1 className="text-2xl">Functional Arrow Components</h1>
-    </>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper items-center">
+        <img src="./hero.png" alt="Hero Banner" />
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
   );
 };
 
